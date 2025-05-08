d25a88adbe8a01898ada794ab10c1d223266e64a
Alex Lopez <alopez@quiknode.io>
2025-05-08 13:07:58 -0400

    add usage endpoints

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const server = new McpServer({
 server.resource(
   "endpoint",
   new ResourceTemplate("endpoint://{name}", {
-    list: 
+    list:
       async (data) => {
         const endpoints = await client.listEndpoints();
         return {
@@ -146,9 +146,66 @@ server.tool(
     start_time: z.number(),
     end_time: z.number(),
   },
-  { description: "Get the usage data for the user's QuickNode RPC endpoints. The start_time and end_time parameters are unix timestamps in seconds to filter the usage data by time range" },
+  { description: "Get the usage data for the user's QuickNode RPC account. The start_time and end_time parameters are unix timestamps in seconds to filter the usage data by time range" },
   async ({ start_time, end_time }) => {
     const usage = await client.getRpcUsage({ start_time, end_time });
+    return {
+      content: [{
+        type: "text",
+        // @ts-ignore
+        text: JSON.stringify(usage.data, null, 2)
+      }]
+    };
+  }
+);
+
+server.tool(
+  "get-rpc-usage-by-endpoint",
+  {
+    start_time: z.number(),
+    end_time: z.number(),
+  },
+  { description: "Get the usage data for the user's QuickNode RPC endpoints. The start_time and end_time parameters are unix timestamps in seconds to filter the usage data by time range" },
+  async ({ start_time, end_time }) => {
+    const usage = await client.getRpcUsageByEndpoint({ start_time, end_time });
+    return {
+      content: [{
+        type: "text",
+        // @ts-ignore
+        text: JSON.stringify(usage.data, null, 2)
+      }]
+    };
+  }
+);
+
+server.tool(
+  "get-rpc-usage-by-method",
+  {
+    start_time: z.number(),
+    end_time: z.number(),
+  },
+  { description: "Get the usage data for the user's QuickNode RPC endpoints, broken down by method. The start_time and end_time parameters are unix timestamps in seconds to filter the usage data by time range" },
+  async ({ start_time, end_time }) => {
+    const usage = await client.getRpcUsageByMethod({ start_time, end_time });
+    return {
+      content: [{
+        type: "text",
+        // @ts-ignore
+        text: JSON.stringify(usage.data, null, 2)
+      }]
+    };
+  }
+);
+
+server.tool(
+  "get-rpc-usage-by-chain",
+  {
+    start_time: z.number(),
+    end_time: z.number(),
+  },
+  { description: "Get the usage data for the user's QuickNode RPC endpoints, broken down by chain. The start_time and end_time parameters are unix timestamps in seconds to filter the usage data by time range" },
+  async ({ start_time, end_time }) => {
+    const usage = await client.getRpcUsageByChain({ start_time, end_time });
     return {
       content: [{
         type: "text",
